c0953d3965761aaf953aaffa01892dfafbc61a27
Domino1234 <122507153+Domino1234B@users.noreply.github.com>
2024-10-18 00:25:08 +0000

    Pending changes exported from your codespace

--- web/eaglercraft_opts.js
+++ web/eaglercraft_opts.js
@@ -3,8 +3,8 @@ window.eaglercraftXOpts = () => ({
   // joinServer: "ws" + window.location.href.slice(4),
   // assetsURI: "custom.epk",
 	servers: [
-		{ addr: "ws" + window.location.href.slice(4), name: "EaglercraftX Server" }
+		{ addr: "ws" + window.location.href.slice(4), name: "CookieCraft" }
 	]
 });
-// document.title = "My Original Server Name:tm:";
-// document.querySelector("link[rel=\"shortcut icon\"]").href = "https://www.google.com/favicon.ico";
+// document.title = "CookieCraft:";
+// document.querySelector("link[rel=\"shortcut icon\"]").href = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngwing.com%2Fen%2Fsearch%3Fq%3Dcookies&psig=AOvVaw0Mx5CpwnvJj9jB0RBlsbwr&ust=1726335145110000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCMjN-9m5wIgDFQAAAAAdAAAAABAE";
